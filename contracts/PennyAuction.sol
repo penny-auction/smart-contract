bc026af4ecb573f40d053ad35fa3fbd3f44dc9a9
Ali Shanaakh <ashanaakh@gmail.com>
2018-12-04 21:46:09 +0200

    Remove return ID in BID

--- contracts/PennyAuction.sol
+++ contracts/PennyAuction.sol
@@ -5,40 +5,34 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 contract PennyAuctionContract is Ownable {
     uint256[] public bidIncrement;
 
-    uint256[] public auctionEnd;
+    // uint256[] public auctionEnd;
     uint256[] public highBid;
 
     string[] public highBidder;
 
-    uint256 public counter;
-    uint256 public timeIncrement = 10 hours;
+    // uint256 public timeIncrement = 10 minutes;
 
     constructor() public {}
 
     function addNewLot(
         uint256 _initialPrice,
         uint256 _bidIncrement
-    ) public onlyOwner returns (uint256 id) {
+    ) public onlyOwner {
         highBidder.push("default"); // Set winner creator of the auction.
         bidIncrement.push(_bidIncrement);
         highBid.push(_initialPrice); // Add min price of the added product.
-        auctionEnd.push(block.timestamp + timeIncrement); // Add time, where auction should finish.
-
-        counter++;
-
-        // Return id of created auction.
-        return counter;
+        // auctionEnd.push(now + timeIncrement); // Add time, where auction should finish.
     }
 
     event Bid(uint256 id, string uid, uint256 highBid);
 
     function bid(uint256 id, string uid) public onlyOwner {
-        require(now < auctionEnd[id], "Auction is not finished.");
+        // require(now < auctionEnd[id], "Auction is finished.");
         require(keccak256(bytes(uid)) != keccak256(bytes(highBidder[id])), "Invalid uid. Already highBidder.");
 
         highBid[id] += bidIncrement[id];
 
-        auctionEnd[id] += timeIncrement;
+        // auctionEnd[id] += timeIncrement;
 
         highBidder[id] = uid;
 
@@ -46,7 +40,7 @@ contract PennyAuctionContract is Ownable {
     }
 
     function getWinner(uint256 id) public view onlyOwner returns (string uid, uint256 bighBid) {
-        require(now < auctionEnd[id], "Auction is not finished.");
+        // require(now > auctionEnd[id], "Auction is not finished.");
 
         return (highBidder[id], highBid[id]);
     }
